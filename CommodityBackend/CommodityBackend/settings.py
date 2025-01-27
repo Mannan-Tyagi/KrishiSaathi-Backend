d5c323859838ee8ea30c44a85099acc977837c7f
Mannan <Mannan@DESKTOP-OTIQOF5>
2025-01-28 00:19:09 +0530

    chore: Update DEBUG setting to True for local development

--- CommodityBackend/CommodityBackend/settings.py
+++ CommodityBackend/CommodityBackend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(q#ona#mfkc8456+!x#=679c!=%76k#k0v&kcl(dq(%3tpiyt1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
